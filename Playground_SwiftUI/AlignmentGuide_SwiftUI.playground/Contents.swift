82800d6652eb5dca38f639cfe2f95e8ab4c39812
taejung2183 <taejung2183@naver.com>
2021-05-14 16:46:27 +0900

    -

--- Playground_SwiftUI/AlignmentGuide_SwiftUI.playground/Contents.swift
+++ Playground_SwiftUI/AlignmentGuide_SwiftUI.playground/Contents.swift
@@ -4,13 +4,14 @@ import PlaygroundSupport
 struct ContentView: View {
   var body: some View {
     // Implement stair-case effect with alignment guide.
+    // You can scale or translate the return value.
     VStack {
       HStack {
         Image(systemName: "swift")
           .resizable()
           .scaledToFit()
           .frame(width: 60)
-          .alignmentGuide(VerticalAlignment.center) { $0[.bottom]}
+          .alignmentGuide(VerticalAlignment.center) { $0[.bottom] * 2 }
         Image(systemName: "swift")
           .resizable()
           .scaledToFit()
@@ -19,7 +20,7 @@ struct ContentView: View {
           .resizable()
           .scaledToFit()
           .frame(width: 60)
-          .alignmentGuide(VerticalAlignment.center) { $0[.top]}
+          .alignmentGuide(VerticalAlignment.center) { $0[.top] - 60 }
       }
     }
   }
